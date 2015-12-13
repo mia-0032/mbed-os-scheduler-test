2f6ca4f84d2de53fe9b91bdfe620b008d0dda61a
Yoshihiro MIYAI <msparrow17@gmail.com>
2015-12-13 23:18:05 +0900

    InterruptInを使ってみた

--- source/main.cpp
+++ source/main.cpp
@@ -4,18 +4,27 @@ using mbed::util::FunctionPointer1;
 using mbed::util::FunctionPointerBind;
 using mbed::util::Event;
 
-static DigitalOut led(LED1);
+static DigitalOut led(LED1, 1);
 
 static void blinky(int i) {
-    led = i;
+    led = i; // 1 => off, 0 => on
+}
+
+static void switch_pressed(void) {
+    FunctionPointer1<void, int> ptr_to_blinky(blinky); // 関数へのポインターの生成
+    FunctionPointerBind<void> bind_of_blinky_on(ptr_to_blinky.bind(0)); // 引数(0)をbind
+    FunctionPointerBind<void> bind_of_blinky_off(ptr_to_blinky.bind(1)); // 引数(1)をbind
+    Event e_on(bind_of_blinky_on); // LEDを光らせるイベントを生成
+    Event e_off(bind_of_blinky_off); // LEDを消すイベントを生成
+
+    // LEDを光らせるイベントをキューへ積む
+    minar::Scheduler::postCallback(e_on);
+    // LEDを消すイベントを1秒後に実行するようにキューへ積む
+    minar::Scheduler::postCallback(e_off).delay(minar::milliseconds(1000));
 }
 
 void app_start(int, char**) {
-    FunctionPointer1<void, int> ptr_to_blinky(blinky); // 関数へのポインターの生成
-    FunctionPointerBind<void> bind_of_blinky_0(ptr_to_blinky.bind(0)); // 引数(0)をbind
-    FunctionPointerBind<void> bind_of_blinky_1(ptr_to_blinky.bind(1)); // 引数(1)をbind
-    Event e_0(bind_of_blinky_0); // LEDを消すイベントを生成
-    Event e_1(bind_of_blinky_1); // LEDを光らせるイベントを生成
-    minar::Scheduler::postCallback(e_0).period(minar::milliseconds(2000)); // 2秒ごとに実行
-    minar::Scheduler::postCallback(e_1).delay(minar::milliseconds(1000)).period(minar::milliseconds(2000)); // 1秒後から2秒ごとに実行
+    // SW2の入力をトリガーにしてswitch_pressedを実行
+    static InterruptIn user_sw(SW2);
+    user_sw.rise(switch_pressed);
 }
